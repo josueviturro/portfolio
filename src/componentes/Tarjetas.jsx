37c9a3d1adfed74d7dc4db2213c3c42db227adf9
josueviturro <josueviturro@gmail.com>
2025-03-12 02:32:07 -0300

    ya casi ya casi

--- src/componentes/Tarjetas.jsx
+++ src/componentes/Tarjetas.jsx
@@ -4,7 +4,7 @@ const Tarjetas = () => {
     return(
         <>{data.map((e) => (
             <div key={e.id} className="Tarjeta">
-            <img className="imagen_tarjeta" src={e.imagen.foto} alt={e.imagen.nombre} />
+            <a className="link_tarjeta" href={e.link}><img className="imagen_tarjeta" src={e.imagen.foto} alt={e.imagen.nombre} /></a>
             <h3 className="tarjeta_titulo">{e.titulo}</h3>
             <p className="tarjeta_descripcion">{e.descripcion}</p>
             <div className="tarjetas_techs">
